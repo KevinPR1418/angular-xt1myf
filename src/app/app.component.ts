39577fbee3d88d34f91585331567789e2c0a0b19
KevinPR1418 <c14180214@john.petra.ac.id>
2020-04-16 19:21:25 +0700

    2 files modified Angular simple try

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Kevin';
+  name = 'World';
+  item = [{
+    nama: 'buah kelengkeng',
+    harga : 10000
+  },{
+    nama:'buah jeruk',
+    harga:9000
+  }];
+
+  arr = [1,2,3,4];
+
+  show : boolean = true;
+
+  user = {
+    nama:'kevin'
+  }
+
+  Pesan = '';
+  Klikbutton(){
+    this.Pesan = 'tombol di tea';
+  }
 }
+
