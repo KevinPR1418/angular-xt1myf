4ea408404929bf0cd39bc0ba2bcd1fc6ccab81cb
KevinPR1418 <c14180214@john.petra.ac.id>
2020-04-23 18:16:23 +0700

    1 file modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { HimeComponent } from './hime/hime.component';
+import { Routes , RouterModule} from '@angular/router'
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, HimeComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/hime/hime.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hime',
+  templateUrl: './hime.component.html',
+  styleUrls: ['./hime.component.css']
+})
+export class HimeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
